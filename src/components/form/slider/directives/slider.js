2a5131ef4cbadf84a348997e9455fdea38c938ec
xufei <xu.fei@outlook.com>
2015-09-15 20:21:34 +0800

    slider

--- src/components/form/slider/directives/slider.js
+++ src/components/form/slider/directives/slider.js
@@ -1,20 +1,139 @@
 import template from "../templates/slider.html";
 
 export default class SliderDirective {
-	constructor() {
+	constructor($document) {
 		this.template = template;
 		this.restrict = "E";
 
 		this.scope = {
-			checked: "=ngModel",
+			value: "=ngModel",
+			min: "=",
+			max: "=",
 			disabled: "="
 		};
+
+		this.$document = $document;
 	}
 
 	link(scope, element, attrs) {
 		this.$scope = scope;
+
+		if (!scope.max) {
+			scope.max = 100;
+		}
+
+		if (!scope.min) {
+			scope.min = 0;
+		}
+
+		element.on("click", function (evt) {
+			var src = evt.srcElement ? evt.srcElement : evt.target;
+
+			if (src.tagName != "DIV") {
+				return;
+			}
+
+			var allWidth = element.children()[0].offsetWidth;
+			var currentWidth = (evt.offsetX || evt.layerX);
+
+			scope.changeValue(Math.round(scope.max * currentWidth / allWidth));
+			scope.$digest();
+		});
+
+		this.$document.on("keypress", function (evt) {
+			if ((evt.keyCode || evt.which) == "45") {
+				scope.decrease();
+				scope.$digest();
+			}
+			else if ((evt.keyCode || evt.which) == "61") {
+				scope.increase();
+				scope.$digest();
+			}
+		});
+
+		var dragging = false;
+		var value = scope.value;
+		var stepperEle = angular.element(element.find("span")[0].firstChild);
+
+		stepperEle.on("mousedown", function () {
+			dragging = true;
+		});
+
+		element.on("mousemove", function (evt) {
+			if (dragging) {
+				var allWidth = element.children()[0].offsetWidth;
+				var currentWidth = evt.clientX - offset(element.find("div")[1]).x;
+
+				var temp = Math.round(scope.max * currentWidth / allWidth);
+				if ((temp >= 0) && (temp <= scope.max)) {
+					value = temp;
+
+					scope.changeValue(value);
+					scope.$digest();
+
+					//stepperEle.css("width", (currentWidth - 1) + "px");
+				}
+			}
+		});
+
+		this.$document.on("mouseup", function () {
+			if (dragging) {
+				//stepperEle.css("width", (value * 100 / scope.max) + "%");
+				scope.changeValue(value);
+				scope.$digest();
+
+				dragging = false;
+			}
+		});
+
+		function offset(element) {
+			var x = 0;
+			var y = 0;
+
+			while (element.offsetParent) {
+				x += element.offsetLeft;
+				y += element.offsetTop;
+
+				element = element.offsetParent;
+			}
+
+			return {x: x, y: y};
+		}
 	}
 
 	controller($scope) {
+		$scope.increase = function () {
+			$scope.changeValue($scope.value + 1);
+		};
+
+		$scope.decrease = function () {
+			$scope.changeValue($scope.value - 1);
+		};
+
+		$scope.valueInRange = function(value) {
+			if (!value) {
+				return true;
+			}
+
+			if ($scope.min) {
+				if (value - $scope.min < 0) {
+					return false;
+				}
+			}
+			if ($scope.max) {
+				if (value - $scope.max > 0) {
+					return false;
+				}
+			}
+			return true;
+		};
+
+		$scope.changeValue = function (value) {
+			if (this.valueInRange(value)) {
+				$scope.value = value;
+			}
+		};
 	}
 }
+
+SliderDirective.$inject = ["$document"];
